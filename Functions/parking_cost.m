7072ab45ed6fe48dbb27e7d4b259149e9b1f6eea
mattreturn1 <mattcozz02@gmail.com>
2026-02-08 15:57:00 +0100

    added comments

--- Functions/parking_cost.m
+++ Functions/parking_cost.m
@@ -1,12 +1,19 @@
 function err = parking_cost(simOut, x_goal, y_goal)
 
-q = simOut.logsout.getElement(1).Values.Data;  % <-- PRIMO segnale
+% Extract the logged state signal (assumed to be q = [x, y, theta])
+q = simOut.logsout.getElement(1).Values.Data;   % first logged signal
+
+% Simulation time vector
 t = simOut.tout;
 
+% Extract Cartesian position
 x = q(:,1);
 y = q(:,2);
 
+% Euclidean distance to the goal at each time instant
 rho = sqrt((x - x_goal).^2 + (y - y_goal).^2);
 
+% Cost: integral of the squared distance over time
 err = trapz(t, rho.^2);
 end
+
