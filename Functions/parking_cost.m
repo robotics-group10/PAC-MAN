ac373efacd2c1b4e1a5090beede8ef1a035b94ce
mattreturn1 <mattcozz02@gmail.com>
2026-02-08 16:02:56 +0100

    update

--- Functions/parking_cost.m
+++ Functions/parking_cost.m
@@ -13,7 +13,8 @@ y = q(:,2);
 % Euclidean distance to the goal at each time instant
 rho = sqrt((x - x_goal).^2 + (y - y_goal).^2);
 
-% Cost: integral of the squared distance over time
-err = trapz(t, rho.^2)/ norm([x_goal,y_goal]);;
+% Cost: integral of the squared distance over time the normalized by
+% initial Eucledian distance
+err = trapz(t, rho.^2)/ norm([x_goal,y_goal]);
 end
 
