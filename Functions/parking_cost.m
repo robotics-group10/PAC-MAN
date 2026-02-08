1d461804ec13d2b0a2bad77ab76f8da08baf0d50
mattreturn1 <mattcozz02@gmail.com>
2026-02-08 15:48:17 +0100

    changed cost function for regulation task

--- Functions/parking_cost.m
+++ Functions/parking_cost.m
@@ -1,18 +1,12 @@
-% Cost function for parking
-% Computes the Euclidean distance between the final position of the vehicle
-% and the desired goal position (x_goal, y_goal). Returns Inf if data is unavailable.
 function err = parking_cost(simOut, x_goal, y_goal)
-    try
-        % Extract the 'q' signal data from the simulation output
-        q_data = simOut.logsout.getElement('q').Values.Data;
-        
-        % Get the final position (x, y) of the vehicle from the last row of q_data
-        final_pos = q_data(end, 1:2);
-        
-        % Compute the Euclidean distance between final position and goal
-        err = sqrt(sum((final_pos - [x_goal y_goal]).^2));
-    catch
-        % If any error occurs (e.g., q data not found), return infinity
-        err = inf;
-    end
+
+q = simOut.logsout.getElement(1).Values.Data;  % <-- PRIMO segnale
+t = simOut.tout;
+
+x = q(:,1);
+y = q(:,2);
+
+rho = sqrt((x - x_goal).^2 + (y - y_goal).^2);
+
+err = trapz(t, rho.^2);
 end
