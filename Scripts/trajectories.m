3817b8dcab7fd019b5d207ba6d905a99d667d6e3
Ale10chine <alex10.chinello@gmail.com>
2026-02-09 18:20:37 +0100

    updates

--- Scripts/trajectories.m
+++
@@ -1,5 +0,0 @@
-    @(t) [2*cos(t), 2*sin(t)];                                      % Circle
-    @(t) [t, sin(t)];                                               % X-linear sine wave
-    @(t) [cos(t), t];                                               % Y-linear sine wave
-    @(t) [ t, 2*tanh(t-5) ];                                        % Step/Lane change trajectory
-    %@(t) [t, 2*(t>5)];                                              % PureStep (not twice differentiable, but can be tried with output error controller if implemented)
