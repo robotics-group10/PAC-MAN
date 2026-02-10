21a3369673df8e82219a79ff1e5eb8f2eb0b4d7b
luca <lucapiai98@proton.me>
2026-02-10 11:03:06 +0100

    Added position error plot on trajectory control

--- Functions/tracking_cost.m
+++ Functions/tracking_cost.m
@@ -1,7 +1,7 @@
 % Cost function for tracking
 % Computes the average tracking error between the actual trajectory (q)
 % and the desired trajectory (q_d) over time.
-function err = tracking_cost(simOut)
+function [err, e] = tracking_cost(simOut)
     % Extract actual position data (x, y) from simulation output
     q  = simOut.logsout.getElement('q').Values.Data(:, 1:2);
     
@@ -10,7 +10,7 @@ function err = tracking_cost(simOut)
 
     % Compute the Euclidean distance (error) at each time step
     e = vecnorm(q - qd, 2, 2);  % 2-norm along each row (each timestep)
-
+    
     % Compute the mean error over all timesteps as the cost
     err = mean(e);
 end
